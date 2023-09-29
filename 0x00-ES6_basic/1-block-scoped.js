fc4bf76b307574f7bfb073b6002b6ea05d45c937
chikadike <dikechika87@gmil.com>
2023-09-29 20:34:39 +0000

    adding file dependencies

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
+  // eslint-disable-next-line no-empty
   if (trueOrFalse) {
-    task = false;
-    task2 = true;
   }
 
   return [task, task2];
 }
-
